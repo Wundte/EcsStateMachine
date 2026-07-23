d4ad937b909545cc5df84a9555aacefacabf684c
Constantine Prokhorov <prokhorovconstantin@gmail.com>
2026-07-23 19:24:20 +0200

    Setup of Test assembly and first test

---
+++ Assets/Tests/StableIdTests.cs
@@ -0,0 +1,17 @@
+﻿using Code.EcsStateMachine.Editor.CodeGeneration.Utils;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class StableIdTests
+    {
+        [Test]
+        public void SameStringProducesSameId()
+        {
+            var first = StableId.Get("Test.Type");
+            var second = StableId.Get("Test.Type");
+            
+            Assert.AreEqual(first, second);
+        }
+    }
+}
